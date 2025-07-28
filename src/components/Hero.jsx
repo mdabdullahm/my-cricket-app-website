6e0db8d9b6ca0c8e3f8b9bd9ba24fbcfb7a38c0b
mdabdullahm <you@example.com>
2025-07-29 00:31:14 +0600

    added visited countries page and local storage feature

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = ({ onClaimCredits }) => {
     <div className="bg-white pt-8">
       <div className="container mx-auto px-6">
         <div className="bg-gradient-to-br from-gray-900 to-slate-800 rounded-2xl text-white text-center py-16 px-8 shadow-2xl">
-          <img src="/public/banner-main.png" alt="Cricket Logo" className="mx-auto h-24 mb-4" />
+          <img src="banner-main.png" alt="Cricket Logo" className="mx-auto h-24 mb-4" />
           <h1 className="text-4xl md:text-5xl font-extrabold mb-3">
             Assemble Your Ultimate Dream 11 Cricket Team
           </h1>
